ab0b4e8df08cd01ef1609993b7a8a231dc21f81c
abhishek_goyal_robomq <abhishek.goyal@robomq.io>
2024-07-28 16:19:52 +0530

    Revert "Initial Commit"
    This reverts commit 4fbaf2aa89e0ba3eef654a96b584531d88221beb.

--- SpringBootJPA/src/main/java/com/springboot/jpa/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.jpa.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.springboot.jpa.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
